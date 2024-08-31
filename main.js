56dab91d983da4aef9ca7624639963ada0a8e958
Aquiles-diaz <aquilesdiaz335@gmail.com>
2024-08-31 20:40:49 -0300

    Slider publicitario, Cards Opciones

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-//Realizar la carga de una nota de un alumno. 
-//Mostrar un mensaje que aprobó si tiene una nota mayor o igual a 4:
+const swiper = new Swiper(".swiper-hero", {
+    // Optional parameters
+    // slidesPerView: "auto",
+    // spaceBetween: 15,
+    // slidesPerGroupAuto: true,
 
-let alumno;
+    direction: "horizontal",
+    loop: true,
+    // allowTouchMove: true,
+    // effect: "cube",
+    autoplay: {
+      delay: 10000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
 
-if (alumno > 4) {
-    
-} else {
-    
-}
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      // type: "progressbar"
+      clickable: true,
+      // dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    //   draggable: true,
+    // },
+  });
